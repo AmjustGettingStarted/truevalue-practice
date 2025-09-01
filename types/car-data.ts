33ee78b80c4f0ca8d414889542b03c3807c4ca2a
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-01 11:04:45 +0530

    feat(cars): Add types for car dummy data
    Co-authored-by: HARSHITH-MV <harshithmv2002@gmail.com>

---
+++ types/car-data.ts
@@ -0,0 +1,11 @@
+// types/car-data.ts
+export type CarType = {
+  status: string;
+  year: number;
+  slug: string;
+  categorytype: string;
+  km: number;
+  fuelType: string;
+  transmission: string;
+  price: number;
+};
