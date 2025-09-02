b4acde517f9a6f29d9ccd4f02fd70fff5f072809
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-02 11:38:05 +0530

    brands data added but logo links are broken/not working
    Co-authored-by: HARSHITH-MV <harshithmv2002@gmail.com>

---
+++ constants/brands.ts
@@ -0,0 +1,77 @@
+export const brands = [
+  {
+    name: "Toyota",
+    img: "https://www.citypng.com/public/uploads/preview/hd-toyota-logo-transparent-png-11637162764xkzjzjv3gk.png",
+    totalCars: 1200,
+  },
+  {
+    name: "Honda",
+    img: "https://freebiesupply.com/logos/honda-logo.png",
+    totalCars: 1100,
+  },
+  {
+    name: "Ford",
+    img: "https://www.citypng.com/public/uploads/preview/ford-logo-emblem-hd-png-11637162764xkzjzjv3gk.png",
+    totalCars: 950,
+  },
+  {
+    name: "Chevrolet",
+    img: "https://www.citypng.com/public/uploads/preview/chevrolet-logo-emblem-png-image-11637162764xkzjzjv3gk.png",
+    totalCars: 900,
+  },
+  {
+    name: "Mercedes-Benz",
+    img: "https://freebiesupply.com/logos/mercedes-benz-logo.png",
+    totalCars: 850,
+  },
+  {
+    name: "BMW",
+    img: "https://freebiesupply.com/logos/bmw-logo.png",
+    totalCars: 800,
+  },
+  {
+    name: "Audi",
+    img: "https://freebiesupply.com/logos/audi-logo.png",
+    totalCars: 780,
+  },
+  {
+    name: "Volkswagen",
+    img: "https://freebiesupply.com/logos/volkswagen-logo.png",
+    totalCars: 750,
+  },
+  {
+    name: "Nissan",
+    img: "https://www.citypng.com/public/uploads/preview/hd-nissan-emblem-logo-transparent-png-11637162764xkzjzjv3gk.png",
+    totalCars: 700,
+  },
+  {
+    name: "Hyundai",
+    img: "https://www.citypng.com/public/uploads/preview/hd-hyundai-logo-transparent-background-11637162764xkzjzjv3gk.png",
+    totalCars: 650,
+  },
+  {
+    name: "Lexus",
+    img: "https://freebiesupply.com/logos/lexus-logo.png",
+    totalCars: 600,
+  },
+  {
+    name: "Mazda",
+    img: "https://www.citypng.com/public/uploads/preview/hd-mazda-logo-emblem-transparent-png-11637162764xkzjzjv3gk.png",
+    totalCars: 580,
+  },
+  {
+    name: "Subaru",
+    img: "https://www.citypng.com/public/uploads/preview/hd-subaru-emblem-logo-sign-png-11637162764xkzjzjv3gk.png",
+    totalCars: 550,
+  },
+  {
+    name: "Jeep",
+    img: "https://www.citypng.com/public/uploads/preview/hd-jeep-emblem-logo-transparent-png-11637162764xkzjzjv3gk.png",
+    totalCars: 520,
+  },
+  {
+    name: "Lamborghini",
+    img: "https://www.citypng.com/public/uploads/preview/hd-lamborghini-logo-transparent-background-11637162764xkzjzjv3gk.png",
+    totalCars: 250,
+  },
+];
