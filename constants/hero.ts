e01d32be1eaad7e6049319273d04ede68d40cfac
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-05 09:35:43 +0530

    fix(hero data): update maxlength
    Co-authored-by: HARSHITH-MV <harshithmv2002@gmail.com>

--- constants/hero.ts
+++ constants/hero.ts
@@ -1,1 +1,7 @@
 export const allCars = ["Brand", "Model", "Year", "Fuel Type", "Price"];
+export const sellCars = [
+  { label: "Enter Reg.No", maxLength: 9 },
+  { label: "Name", maxLength: 20 },
+  { label: "Mobile", maxLength: 10 },
+  { label: "Email Address", maxLength: 20 },
+];
