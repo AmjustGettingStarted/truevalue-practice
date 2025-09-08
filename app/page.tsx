fd12e2d3509646a63f2eb8fb39626e53e10aaf0c
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-08 09:46:31 +0530

    Add comment
    Co-authored-by: HARSHITH-MV <harshithmv2002@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BrowseByBrands from "@/components/brands/browse-brands";
 import Hero from "@/components/hero/hero";
 import UsedCars from "@/components/used-cars/used-cars";
 
 export default function Home() {
   return (
     <>
+      {/* Hero Section  */}
       <Hero />
       <UsedCars />
     </>
